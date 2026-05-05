83d77c293ee045c526b40362dd1a06d8bf6b9b2f
Domagoj <domagojboksic@gmail.com>
2026-05-05 14:23:10 +0200

    final final

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,21 @@
 fetch("data.json")
 .then(function(response) {
-    console.log(response);
+    return response.json();
+})
+.then(function(data) {
+    console.log(data);
+
+    let sum = 0;
+    let i = 0;
+    for(i = 0; i < data.length; i++) {
+        let string = data[i].category.toLowerCase()
+        let category = document.querySelector(`#${string}`);
+        let score = category.querySelector(`.score .your-score`)
+        
+        score.textContent = data[i].score;
+        sum += data[i].score;
+    }
+
+    let overallScore = document.querySelector('.overall-result .your-result h1')
+    overallScore.textContent = Math.floor(sum / i);
 });
